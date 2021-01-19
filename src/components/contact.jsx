1143e338dcfe700e0d5ff3fb384a3425e9d592a3
chobrandone <chobrandone4@gmail.com>
2021-01-19 16:32:37 +0100

    last commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -117,9 +117,9 @@ export class Contact extends Component {
         <div id="footer">
           <div className="container text-center">
             <p>
-              &copy; 2020 Issaaf Kattan React Land Page Template. Design by{" "}
-              <a href="http://www.templatewire.com" rel="nofollow">
-                TemplateWire
+              &copy; 2021 ItneXt. Design by{" "}
+              <a href="#brandone" rel="nofollow">
+                Cho Brandone
               </a>
             </p>
           </div>
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -8,22 +8,21 @@ export class Services extends Component {
           <div className="section-title">
             <h2>Our Services</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-              dapibus leonec.
+              ItneXt provide you with the following techonolgial services
             </p>
           </div>
           <div className="row">
             {this.props.data
               ? this.props.data.map((d, i) => (
-                  <div  key={`${d.name}-${i}`} className="col-md-4">
-                    {" "}
-                    <i className={d.icon}></i>
-                    <div className="service-desc">
-                      <h3>{d.name}</h3>
-                      <p>{d.text}</p>
-                    </div>
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
                   </div>
-                ))
+                </div>
+              ))
               : "loading"}
           </div>
         </div>
